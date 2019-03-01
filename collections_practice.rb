b3a9bd991ac0c0053d1d3ad47e8ef8a874c97c7c
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-01 17:52:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
-# your code goes here
+def begins_with_r(array)
+  answer = false  
+  array.each do |item|
+    if item.start_with?("r")
+      answer = true 
+    else 
+      answer = false 
+    end 
+  end
+  answer 
+end
+
+def contain_a(array)
+  new_array = []
+  array.each do |item| 
+  if item.include?("a") 
+    new_array.push("#{item}")
+  end 
+end
+new_array
+end 
+
+def first_wa(array)
+  new_array = []
+  answer = test 
+  array.each do |item|
+    if item.is_a?(String)
+    new_array.push("#{item}")
+  end
+ end
+  new_array.each do |thing|
+    if thing.include?("wa")
+      answer = thing   
+    end 
+end 
+answer
+end 
+ 
+
+
+
